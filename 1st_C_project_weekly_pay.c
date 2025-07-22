783130983c2be0da0120429503940353f79b5a6e
TYLER <tyler67676767@gmail.com>
2025-07-22 23:07:52 +0800

    1st_C_project_weekly_pay.c

--- 1st_C_project_weekly_pay.c
+++ 1st_C_project_weekly_pay.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    double yearlysalary, monthlysalary, weeklysalary, dailysalary; //double is simply a more precise version of floats
+    double yearlysalary, monthlysalary, weeklysalary, dailysalary;
 if (yearlysalary <=0) {
     printf("Invalid input. Please enter a positive number to calcuulate your salary\n");
 }
     printf("input your yearly salary: ");
-    scanf("%lf", &yearlysalary); //scanf is used to get user input, %lf is for to expect a double as input
+    scanf("%lf", &yearlysalary);
 
     monthlysalary = yearlysalary/12.0;
     weeklysalary = yearlysalary/52.0;
